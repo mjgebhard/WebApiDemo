6ac9a3fd3a65f478e2a2054023a870011cdf1d23
Mike Gebhard <Mike.Gebhard@dpi.nc.gov>
2022-06-22 06:19:06 -0400

    Added client post, put and delete

--- ClientConsole/Program.cs
+++ ClientConsole/Program.cs
@@ -17,7 +17,7 @@ namespace ClientConsole
             client = new HttpClient();
             client.BaseAddress = new Uri("https://localhost:44328");
 
-            //Make the HTTP request
+            //Make an HTTP GET request
             List<ResponseOrder> data = GetData().GetAwaiter().GetResult();
 
             //Display List<ResponseOrder> values
@@ -25,14 +25,38 @@ namespace ClientConsole
             {
                 Console.WriteLine(item.taskname);
             }
+            Console.WriteLine();
+
+            //Make an HTTP GET request by Id
+            ResponseOrder data1 = GetData("2022-06-10-O424").GetAwaiter().GetResult();
+            Console.WriteLine(data1.taskname);
+            Console.WriteLine();
+
+            //Make an HTTP POST request
+            ResponseOrder data2 = PostData().GetAwaiter().GetResult();
+            Console.WriteLine(data2.taskname);
+            Console.WriteLine();
+
+            //Make an HTTP PUT request
+            ResponseOrder data3 = PutData().GetAwaiter().GetResult();
+            Console.WriteLine(data3.taskname);
+            Console.WriteLine();
+
+            //Make an HTTP Delete request
+            DeleteData("2022-06-10-O424").GetAwaiter().GetResult();
 
         }
 
         public static async Task<List<ResponseOrder>> GetData()
         {
-            HttpResponseMessage response = await client.GetAsync("api/values");
+            Console.WriteLine();
+            Console.WriteLine("****  GET ****");
+            Console.WriteLine();
+
             List<ResponseOrder> data = new List<ResponseOrder>();
             string rawJson = string.Empty;
+
+            HttpResponseMessage response = await client.GetAsync("api/values");
 
             if (response.IsSuccessStatusCode)
             {
@@ -48,7 +72,139 @@ namespace ClientConsole
             return data;
         }
 
+        public static async Task<ResponseOrder> GetData(string id)
+        {
+            Console.WriteLine();
+            Console.WriteLine("****  GET by Id ****");
+            Console.WriteLine();
 
+            ResponseOrder data = new ResponseOrder();
+            string rawJson = string.Empty;
+
+            HttpResponseMessage response = await client.GetAsync($"api/values/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                //Display the raw data
+                rawJson = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(rawJson);
+                Console.WriteLine();
+
+                //Deserialize into the List<ResponseOrder> type
+                data = JsonConvert.DeserializeObject<ResponseOrder>(rawJson);
+            }
+
+            return data;
+        }
+
+        public static async Task<ResponseOrder> PostData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("****  Post ****");
+            Console.WriteLine();
+
+            ResponseOrder data = new ResponseOrder();
+            string rawJson = string.Empty;
+
+            ResponseOrder request = new ResponseOrder()
+            {
+                actorname = "Marking",
+                stepname = "2022-06-10-O424",
+                taskname = "EXECUTION_2",
+                parameters = new List<Parameter>
+                    {
+                      new Parameter {  name = "parameter A", value = 73.1m },
+                      new Parameter {  name = "parameter B", value = 12m },
+                      new Parameter {  name = "parameter C", value = 173.1m },
+                      new Parameter {  name = "parameter D", value = 112m },
+                      new Parameter {  name = "parameter E", value = 273.1m },
+                      new Parameter {  name = "parameter F", value = 312m },
+                    }
+            };
+
+            string json = JsonConvert.SerializeObject(request);
+            StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("api/values", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //Display the raw data
+                rawJson = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(rawJson);
+                Console.WriteLine();
+
+                //Deserialize into the List<ResponseOrder> type
+                data = JsonConvert.DeserializeObject<ResponseOrder>(rawJson);
+            }
+
+            return data;
+
+        }
+
+        public static async Task<ResponseOrder> PutData()
+        {
+            Console.WriteLine();
+            Console.WriteLine("****  Put ****");
+            Console.WriteLine();
+            ResponseOrder data = new ResponseOrder();
+            string rawJson = string.Empty;
+
+            ResponseOrder request = new ResponseOrder()
+            {
+                actorname = "Marking",
+                stepname = "2022-06-10-O424",
+                taskname = "EXECUTION_2",
+                parameters = new List<Parameter>
+                    {
+                      new Parameter {  name = "parameter A", value = 73.1m },
+                      new Parameter {  name = "parameter B", value = 12m },
+                      new Parameter {  name = "parameter C", value = 173.1m },
+                      new Parameter {  name = "parameter D", value = 112m },
+                      new Parameter {  name = "parameter E", value = 273.1m },
+                      new Parameter {  name = "parameter F", value = 312m },
+                    }
+            };
+
+            string id = request.stepname;
+
+            string json = JsonConvert.SerializeObject(request);
+            StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync($"api/values/{id}", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //Display the raw data
+                rawJson = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(rawJson);
+                Console.WriteLine();
+
+                //Deserialize into the List<ResponseOrder> type
+                data = JsonConvert.DeserializeObject<ResponseOrder>(rawJson);
+            }
+
+            return data;
+
+        }
+
+        
+        public static async Task DeleteData(string id)
+        {
+            Console.WriteLine();
+            Console.WriteLine("****  Delte ****");
+            Console.WriteLine();
+            string rawJson = string.Empty;
+
+            HttpResponseMessage response = await client.DeleteAsync($"api/values/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                //Display the raw data
+                rawJson = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(rawJson);
+                Console.WriteLine();
+            }
+
+        }
 
     }
 }
